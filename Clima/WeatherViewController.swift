961fae11ec772a34ed5a194e3e101a0e832cb28f
MarySalemme <marysalemme1@gmail.com>
2019-02-18 13:19:13 +0000

    Add API key for Open Weather

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -10,16 +10,13 @@ import UIKit
 
 
 class WeatherViewController: UIViewController {
-    
+
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
-    /***Get your own App ID at https://openweathermap.org/appid ****/
+    let APP_ID = Constants.API_KEY
     
-
     //TODO: Declare instance variables here
     
-
     
     //Pre-linked IBOutlets
     @IBOutlet weak var weatherIcon: UIImageView!
